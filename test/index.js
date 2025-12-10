420ddd54a7bc60052e026932f5878ec720aa0a4a
Michael Smith <owlstronaut@github.com>
2025-12-10 15:33:38 -0800

    chore: swap from tap to built-in node:test

--- test/index.js
+++ test/index.js
@@ -1,5 +1,15 @@
 const nff = require('../')
-const t = require('tap')
-t.equal(nff('/a/b/c/@foo/bar'), '@foo/bar')
-t.equal(nff('/a/b/c/foo/bar'), 'bar')
-t.equal(nff(null), false)
+const { test } = require('node:test')
+const assert = require('node:assert')
+
+test('extracts scoped package name', () => {
+  assert.strictEqual(nff('/a/b/c/@foo/bar'), '@foo/bar')
+})
+
+test('extracts unscoped package name', () => {
+  assert.strictEqual(nff('/a/b/c/foo/bar'), 'bar')
+})
+
+test('returns false for null input', () => {
+  assert.strictEqual(nff(null), false)
+})
